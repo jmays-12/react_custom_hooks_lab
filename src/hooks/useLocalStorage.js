a371d50e3511b7d553fdfbe2fa8546df8a56967f
Jordan Mays <jmays12@gmail.com>
2026-07-15 16:18:37 -0500

    created useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+    const getStore = localStorage.getItem(key);
+
+    const [store, setStore] = useState(getStore ? getStore : initialValue);
+
+    useEffect(() => {
+        localStorage.setItem(key, store);
+    }, [key, store]);
+
+    return [store, setStore];
 }
